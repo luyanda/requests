b9dc10920af449f73a7ae96f6a3bf965a13e6c1f
steynvl <steyn.van.l@gmail.com>
2018-03-25 19:01:27 +0200

    Fixed issue in requests/compat.py

--- requests/compat.py
+++ requests/compat.py
@@ -20,8 +20,8 @@ import sys
 _ver = sys.version_info
 
 #: Python 2.x?
-is_py2 = (_ver[0] == 1)
-# is_py2 = (_ver[0] == 2)
+# is_py2 = (_ver[0] == 1)
+is_py2 = (_ver[0] == 2)
 
 #: Python 3.x?
 is_py3 = (_ver[0] == 3)
